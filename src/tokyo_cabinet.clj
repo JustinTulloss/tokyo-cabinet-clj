cc829281d7918b476cd4dc76b2472d409882e3d8
Justin Tulloss <jmtulloss@gmail.com>
2009-03-22 23:13:54 -0700

    Now support all of the different types of databases by keyword

--- src/tokyo_cabinet.clj
+++ src/tokyo_cabinet.clj
@@ -4,18 +4,32 @@
 
 (ns tokyo-cabinet
     ; exclude symbols we'll use in our API
-    (:refer-clojure :exclude [use get]))
+    (:refer-clojure :exclude [use get])
+    (:import (tokyocabinet HDB FDB BDB)))
 
 
 (declare *db*)
+
+(def *db-types* {
+    :bdb BDB,
+    :fdb FDB,
+    :hdb HDB
+})
+
+(defmacro getStatic [c f] 
+    `(.get (.getField ~c (name '~f)) ~c))
 
 (defn get [key] (.get *db* key))
 
 (defn put [key value] (.put *db* key value))
 
-(defmacro use [filename & body]
-    (import '(tokyocabinet HDB))
-    `(with-open [hdb# (HDB.)]
-        (.open hdb# ~filename (bit-or HDB/OWRITER HDB/OCREAT))
-        (binding [*db* hdb#]
-            (do ~@body))))
+(defmacro use [filename type & body]
+    (let [klass (type *db-types*)]
+        (import '(tokyocabinet HDB))
+        (println (.newInstance klass))
+        `(with-open [db# (.newInstance ~klass)]
+            (.open db# ~filename (bit-or 
+                (getStatic ~klass OWRITER)
+                (getStatic ~klass OCREAT)))
+            (binding [*db* db#]
+                (do ~@body)))))
